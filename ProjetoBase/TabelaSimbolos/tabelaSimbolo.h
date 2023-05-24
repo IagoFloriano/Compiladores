b412c944b128ebab79e5efc269038962bb1ac3fb
IagoFloriano <iagomfloriano@gmail.com>
2023-05-24 18:47:30 -0300

    .h da tabela simbolos

--- ProjetoBase/TabelaSimbolos/tabelaSimbolo.h
+++ ProjetoBase/TabelaSimbolos/tabelaSimbolo.h
@@ -3,5 +3,23 @@
 
 #include "simbolo.h"
 
+typedef struct tabela {
+  int tam;
+  int topo;
+  struct *simbolo;
+} tabela;
+
+void inicializa(tabela *t);
+
+void push(tabela *t, simbolo s);
+
+simbolo pop(tabela *t);
+
+void removeN(tabela *t, int n);
+
+simbolo *busca(tabela *t, char *ident);
+
+void atribuiTipo(tabela *t, int tipo, int num);
+
 
 #endif // __TABELA_SIMBOLO__
